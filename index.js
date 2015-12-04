5de85c7f97040688f132deb557cc811111dce076
zigur <massimorgon@gmail.com>
2015-12-04 13:16:15 +0100

    added custom errors

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
  * @author Massimiliano Izzo
  */
 var ControllerOut = require('./lib/ControllerOut.js');
+var Errors = require('./lib/Errors.js');
 module.exports.ControllerOut = ControllerOut;
+module.exports.Errors = Errors;
